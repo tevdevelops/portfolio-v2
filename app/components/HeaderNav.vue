a6a4cafb97354287ffa8d8122af2b63bb9e25e46
Tevin Rivera <tevin@bradsdeals.com>
2025-06-10 18:23:45 -0600

    Well we gotta start with a commit somewhere lol
    - we init the project with @nuxt/content starter
    - added some sass depencies, file structure, vars, mixins, other hoopla
    hmm what else
    - started a headernav component
    - add index and about markdown pages. more to come
    
    probably some other stuff but yeah gonna call it here. and better
    chunked commits should be coming after this

---
+++ app/components/HeaderNav.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <a class="skip-link" href="#main">Skip to main content</a>
+    Logo
+    <nav aria-label="Main Navigation">
+      <ul role="menu">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/portfolio">Portfolio</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #f8f9fa;
+  position: sticky;
+  top: 0;
+  width: 100%;
+}
+
+ul {
+  display: flex;
+  justify-content: space-between;
+  gap: 0.5rem;
+}
+
+.skip-link {
+  position: absolute;
+  left: -999px;
+}
+.skip-link:focus {
+  left: 1rem;
+  top: 1rem;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxtjs/color-mode',
+  ],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-04-03',
+
+  alias: {
+    '@': './',
+    '@assets': './assets',
+  },
+  
+  css: ['@assets/css/main.scss'],
+
+  colorMode: {
+    classSuffix: '', // so it sets `data-theme="dark"` not `data-theme-dark`
+  },
+})
