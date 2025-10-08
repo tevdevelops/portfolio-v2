e47541f2c7f6d6dacb63564287b842af2cf5d1a1
Tevin Rivera <tevin@bradsdeals.com>
2025-10-08 14:12:17 -0600

    - add max-width to the site for >xl devices

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -7,12 +7,23 @@
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@use 'styles/helpers' as *;
+
 .layout-container {
+  @include respond(xl) {
+    max-width: 1280px;
+    margin-left: auto;
+    margin-right: auto;
+  }
 }
 
 .main-content {
   padding-top: 1rem;
   height: 100%;
+
+  @include respond(xl) {
+    padding-top: 2.5rem;
+  }
 }
 </style>
